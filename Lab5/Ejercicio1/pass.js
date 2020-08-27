92602ce15d40c76494346572006f417a2a5a4f77
Sebastian R <sebas.r.2599@gmail.com>
2020-08-26 23:57:33 -0500

    Entrega

---
+++ Lab5/Ejercicio1/pass.js
@@ -0,0 +1,6 @@
+let clickpass = document.getElementById("pwdButt").addEventListener("click",comparePass);
+
+function comparePass(){
+    let str =document.getElementById("pwd").value;
+    console.log(str);
+}

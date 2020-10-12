ce4ef00aa883e67cdd77a721d3e71be2c86ba984
Sebastian R <sebas.r.2599@gmail.com>
2020-10-11 20:36:50 -0500

    Creacion Lab18 y hotfix Lab15

---
+++ Lab18/fruits.sql
@@ -0,0 +1,12 @@
+CREATE TABLE fruits(
+    name varchar(50),
+    quantity int(3),
+    price int(4),
+    PRIMARY KEY(name)
+)
+
+INSERT INTO `fruits`(`name`, `quantity`, `price`) VALUES ('Uva',20,28),
+INSERT INTO `fruits`(`name`, `quantity`, `price`) VALUES ('Manzana',5,40),
+INSERT INTO `fruits`(`name`, `quantity`, `price`) VALUES ('Aguacate',4,60),
+INSERT INTO `fruits`(`name`, `quantity`, `price`) VALUES ('Naranja',8,35),
+INSERT INTO `fruits`(`name`, `quantity`, `price`) VALUES ('Ciruela',6,38)
